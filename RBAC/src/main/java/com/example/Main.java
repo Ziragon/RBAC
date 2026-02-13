046f6039ac4f648679765845088b0cca46dff0ff
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2026-02-13 19:15:53 +0700

    feat: add user validation testing

--- RBAC/src/main/java/com/example/Main.java
+++ RBAC/src/main/java/com/example/Main.java
@@ -1,7 +1,39 @@
 package com.example;
 
+import com.example.entity.User;
+
 public class Main {
     static void main() {
-        IO.println(String.format("Hello and welcome!"));
+        // дефолтное создание
+        User user1 = new User("zhu_yuan", "Zhu Yuan", "email@example.com");
+        System.out.println(user1.format());
+
+        // null username
+        try {
+            new User(null, "Zhu Yuan", "email@example.com");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        // короткий username
+        try {
+            new User("zu", "Zhu Yuan", "email@example.com");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        // неразрешенные символы в username
+        try {
+            new User("юзер", "Zhu Yuan", "email@example.com");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        // email без точки после @
+        try {
+            new User("zhu_yuan", "Zhu Yuan", "email@example");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
